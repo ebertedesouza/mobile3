e30c33905b53f3aedd652ae47be7e820bf4378db
Eberte <ebertesouza16@gmail.com>
2025-05-26 10:52:38 -0300

    Atualização do projeto após alterações locais

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
 
-  baseURL: '192.168.15.4:3000'
- // baseURL: 'https://backpizza-seven.vercel.app/'
+  baseURL: 'https://backpizza-seven.vercel.app/'
+  
 })
 
 export { api };
